5edd26d13c8874a256a28fd8e4893e01f68f0703
HarborYuan <yuanhaobo@whu.edu.cn>
2019-04-20 20:37:36 +0800

    add index.html

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -1,17 +1,25 @@
 package handlers
 
 import (
+	"io/ioutil"
 	"log"
 	"net/http"
 )
 
 func Handler() {
 	staticServer := http.FileServer(http.Dir("static"))
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		log.Print("asd")
-	})
+	http.Handle("/static/", http.StripPrefix("/static/", staticServer))
 	http.HandleFunc("/favicon.ico", func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, "static/icon/favicon.ico")
 	})
-	http.Handle("/static/", http.StripPrefix("/static/", staticServer))
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		log.Print("Handling / : " + "'" + r.URL.Path + "'")
+		indexFile, err := ioutil.ReadFile("templates/index.html")
+		if err != nil {
+			log.Print("Handler() : What's wrong with index.html?")
+			w.WriteHeader(http.StatusBadGateway)
+			return
+		}
+		_, err = w.Write(indexFile)
+	})
 }
--- views/index.go
+++
@@ -1,7 +0,0 @@
-package views
-
-import "net/http"
-
-func SendIndex(w http.ResponseWriter, r *http.Request) {
-
-}
